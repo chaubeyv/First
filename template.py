046e09799b2aaab7b4ae330d457fd4a2e5d13a2b
vinit chaubey <chaubeyvinit02@gmail.com>
2024-03-12 19:17:12 +0530

    structure_updated

---
+++ template.py
@@ -0,0 +1,39 @@
+import os 
+from pathlib import Path
+
+
+list_of_files=[
+    ".githhub/workflows/.gitkeep",
+    "src/_init_.py",
+    "src/components/_init_.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/_init_.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_init_.py",
+    "src/utlis/_init_.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception",
+    "tests/unit/_init_.py",
+    "tests/integration/_init_.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb",
+
+]
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    fildir,filename=os.path.split(filepath)
+    if fildir !="":
+        os.makedirs(fildir,exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass # create an empty file
